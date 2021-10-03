33abeeea31ce10ea2453e9c9bcd60c8d370dbe71
Endru Reza <lotusb13@gmail.com>
2021-10-04 01:02:28 +0700

    chore: add env and config

---
+++ config/app.js
@@ -0,0 +1,7 @@
+export default {
+  name: process.env.APP_NAME || 'Node Structure',
+  env: process.env.APP_ENV || 'production',
+  url: process.env.APP_URL || 'http://localhost',
+  debug: process.env.APP_DEBUG || false,
+  port: process.env.APP_PORT || 3000
+}
---
+++ config/database.js
@@ -0,0 +1,21 @@
+export default {
+  default: process.env.DB_CONNECTION || 'mysql',
+  connection: {
+    mysql: {
+      driver: 'mysql',
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: process.env.DB_PORT || '3306',
+      database: process.env.DB_DATABASE || 'node',
+      username: process.env.DB_USERNAME || 'node',
+      password: process.env.DB_PASSWORD || ''
+    },
+    pgsql: {
+      driver: 'pgsql',
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: process.env.DB_PORT || '5432',
+      database: process.env.DB_DATABASE || 'node',
+      username: process.env.DB_USERNAME || 'node',
+      password: process.env.DB_PASSWORD || ''
+    }
+  }
+}
